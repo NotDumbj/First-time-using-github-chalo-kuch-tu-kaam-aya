ce037e2f8d838dabe0e2317c8e12dccc56141efb
daniyal526 <146337576+daniyal526@users.noreply.github.com>
2023-11-23 20:28:29 +0500

    Update daniyal.cpp

--- First time using github chalo kuch tu kaam aya/daniyal.cpp
+++ First time using github chalo kuch tu kaam aya/daniyal.cpp
@@ -1,6 +1,12 @@
-int discount(int price, int quantity)
+int totalprice(int totalprice)
+{
+}
+
+int discount(int price, int quantity,int salestaxrate)
 { int discount;
  discount=price-(price*0.10);
  discount*=quantity;
+  salestaxrate=salestaxrate/100;
+ 
 return discount; 
 }
